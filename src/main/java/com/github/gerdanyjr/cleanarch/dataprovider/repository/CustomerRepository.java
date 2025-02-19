e92e72b418bd5403bcbfe3450d9d040abe55a143
GerdanyJr <asa_lindo@hotmail.com>
2025-02-19 19:11:32 -0300

    fix: fix the MongoRepository type annotation

--- src/main/java/com/github/gerdanyjr/cleanarch/dataprovider/repository/CustomerRepository.java
+++ src/main/java/com/github/gerdanyjr/cleanarch/dataprovider/repository/CustomerRepository.java
@@ -1,8 +1,9 @@
 package com.github.gerdanyjr.cleanarch.dataprovider.repository;
 
+import com.github.gerdanyjr.cleanarch.dataprovider.repository.entity.CustomerEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends MongoRepository<CustomerRepository, String> {
+public interface CustomerRepository extends MongoRepository<CustomerEntity, String> {
 }
