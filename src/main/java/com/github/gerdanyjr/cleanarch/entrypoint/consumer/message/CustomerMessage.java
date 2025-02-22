52e20025f874d4138b6b72682fc01a374f017f7c
GerdanyJr <asa_lindo@hotmail.com>
2025-02-22 15:58:13 -0300

    feat: add kafka configuration

---
+++ src/main/java/com/github/gerdanyjr/cleanarch/entrypoint/consumer/message/CustomerMessage.java
@@ -0,0 +1,21 @@
+package com.github.gerdanyjr.cleanarch.entrypoint.consumer.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerMessage {
+    private String id;
+
+    private String name;
+
+    private String zipCode;
+
+    private String cpf;
+
+    private Boolean isValidCpf;
+
+}
